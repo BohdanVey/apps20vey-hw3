eae1e7072df387dd175be3c4c820bc0ced175d24
BohdanVey <bogdan.vey2002@gmail.com>
2020-11-27 23:31:32 +0200

    SmartArray initial commit

--- src/main/java/ua/edu/ucu/smartarr/FilterDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/FilterDecorator.java
@@ -1,18 +1,18 @@
 package ua.edu.ucu.smartarr;
 
-import ua.edu.ucu.functions.MyFunction;
 import ua.edu.ucu.functions.MyPredicate;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 // Tests every element and removes it if it doesn't satisfy MyPredicate
-public class FilterDecorator extends SmartArrayDecorator{
+public class FilterDecorator extends SmartArrayDecorator {
     public FilterDecorator(SmartArray smartArray, MyPredicate pred) {
         super(smartArray);
-        this.decoratedArray = Arrays.stream(smartArray.toArray()).filter(pred   ::test).toArray();
+        this.decoratedArray =
+                Arrays.stream(smartArray.toArray()).filter(pred::test).toArray();
 
     }
+
     @Override
     public String operationDescription() {
         return "Filter our Array";
