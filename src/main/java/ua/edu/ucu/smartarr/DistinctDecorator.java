2f52a574dc729c9a9a293f9c9d5117748c143440
BohdanVey <bogdan.vey2002@gmail.com>
2020-11-27 23:26:52 +0200

    SmartArray initial commit

--- src/main/java/ua/edu/ucu/smartarr/DistinctDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/DistinctDecorator.java
@@ -3,19 +3,20 @@ package ua.edu.ucu.smartarr;
 import java.util.ArrayList;
 
 // Remove duplicates from SmartArray. Use method equals() to compare objects
-public class  DistinctDecorator extends SmartArrayDecorator {
+public class DistinctDecorator extends SmartArrayDecorator {
     public DistinctDecorator(SmartArray smartArray) {
         super(smartArray);
         Object[] array = smartArray.toArray();
         ArrayList<Object> copiedArr = new ArrayList<>();
-        for(int i=0;i<array.length;i++){
-            for(int j=i+1;j<array.length;j++){
-                if(array[i].equals(array[j])){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].equals(array[j])) {
 
-                   break;
+                    break;
                 }
-                if(j == array.length - 1)
+                if (j == array.length - 1) {
                     copiedArr.add(array[i]);
+                }
             }
         }
 
@@ -24,6 +25,7 @@ public class  DistinctDecorator extends SmartArrayDecorator {
 
     @Override
     public String operationDescription() {
-        return "Remove duplicates using equals, without changing the order(we don't use sort)";
+        return "Remove duplicates using equals, without changing" +
+                " the order(we don't use sort)";
     }
 }
